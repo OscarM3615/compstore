645124971cfdf35ba05783e3ccb405c903b3a8aa
Óscar Miranda <oscarmiranda3615@gmail.com>
2021-10-25 18:48:20 -0600

    fix: force 4:3 ratio on images

--- src/shared/components/ProductCard.tsx
+++ src/shared/components/ProductCard.tsx
@@ -8,13 +8,24 @@ type PropType = {
 const ProductCard = ({ product }: PropType) => {
 	return (
 		<div className="card h-100">
-			<img className="card-img-top" src={product.imageUrl} alt={product.name} />
 			<div className="card-body">
+				<div className="ratio ratio-4x3 mb-3">
+					<img
+						className="card-img-top object-fit-contain"
+						src={product.imageUrl}
+						alt={product.name}
+					/>
+				</div>
 				<p className="h5 card-title">{product.name}</p>
 				<p className="h6 card-subtitle text-success">$ {product.price}</p>
 			</div>
 			<div className="card-footer d-flex align-items-center justify-content-between">
-				<span className="badge bg-secondary">{product.category.name}</span>
+				<Link
+					to={`/categories/${product.category.id}`}
+					className="badge bg-secondary text-decoration-none text-light"
+				>
+					{product.category.name}
+				</Link>
 
 				<Link
 					to={`/products/${product.id}`}
